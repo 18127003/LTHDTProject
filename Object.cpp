c9be15d397082148b43b47363adde4e537c7eebb
hoangnhan1105 <51204146+hoangnhan1105@users.noreply.github.com>
2019-07-19 10:44:39 +0700

    Add files via upload

---
+++ Object.cpp
@@ -0,0 +1,125 @@
+#include "pch.h"
+#include "Object.h"
+
+
+Object::Object()
+{
+
+}
+
+Object::Object(const Object& other)
+{
+	position = other.position;
+	texture = other.texture;
+	tile = other.tile;
+	isMoving = other.isMoving;
+	dir = other.dir;
+	sound = other.sound;
+	skin = other.skin;
+	moveSpeed = other.moveSpeed;
+	timer = other.timer;
+}
+
+Object::~Object()
+{
+}
+
+void Object::self_draw()
+{
+	if (dir == LEFT)
+	{
+		G_Draw(texture[skin], &position);
+	}
+	else G_DrawEx(texture[skin], &position, SDL_FLIP_HORIZONTAL);
+}
+/*
+void Object::self_load(const char *file, enum ObjectType otype,int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	type = otype;
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+	if (otype == EAGLE) moveSpeed = 20;
+	if (otype == COIN) dir = LEFT;
+}
+*/
+void Object::add_texture(const char *file)
+{
+	texture.push_back(G_LoadImage(file));
+}
+/*
+void Object::load_player(const char *file, G_Rect pos, const char *sfile)
+{
+	texture.push_back(G_LoadImage(file));
+	position = pos;
+	type = PLAYER;
+	sound = G_LoadSound(sfile);
+	//isMoving = true;
+}
+*/
+void Object::change_skin(size_t n)
+{
+	if (n >= texture.size()) n = 0;
+	skin = n;
+}
+
+
+void Player::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+
+}
+void CAR::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+}
+void STICK::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+}
+void LAMP::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+}
+void TRAIN::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+}
+void EAGLE::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+	moveSpeed = 20;
+}
+void COIN::self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move)
+{
+	texture.push_back(G_LoadImage(file));
+	position = { pos1,pos2,posx,posy };
+	isMoving = Is_move;
+	sound = G_LoadSound(sfile);
+	dir = LEFT;
+}
+void Player::load_player(const char *file, G_Rect pos, const char *sfile)
+{
+	texture.push_back(G_LoadImage(file));
+	position = pos;
+	sound = G_LoadSound(sfile);
+}
---
+++ Object.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "Genio.h"
+#include <vector>
+using namespace std;
+//enum ObjectType { PLAYER, CAR, TRAIN, STICK, LAMP, EAGLE, COIN};
+enum Direction { UP, DOWN, LEFT, RIGHT };
+class Object
+{
+protected:
+	G_Rect position;
+	vector<G_Texture*> texture;
+	//ObjectType type;
+	SDL_Point tile;
+	bool isMoving = false;
+	Direction dir;
+	G_Sound* sound = NULL;
+	size_t skin = 0;
+	union { //All of Objects Need MoveSpeed Except Train. The Speed of Train Should Be Constant. Instead of Speed , it has a Timer to show when it should come!!!
+		int moveSpeed;
+		int timer;
+	};
+public:
+	void self_draw(); //G_draw
+	virtual void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move) = 0;
+	bool Is_move() { return isMoving; }
+	//void load_player(const char *file, G_Rect pos, const char *sfile);
+	void add_texture(const char *file);
+	void change_skin(size_t n);
+	Object();
+	Object(const Object& other);
+	~Object();
+	friend class Game;
+};
+
+class Player : public Object
+{
+public:
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+	void load_player(const char *file, G_Rect pos, const char *sfile);
+};
+class CAR :public Object
+{
+public:
+	CAR() = default;
+	CAR(const Object* other) : Object(*other) {}
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+};
+class TRAIN :public Object
+{
+public:
+	TRAIN() = default;
+	TRAIN(const Object* other) : Object(*other) {}
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+};
+class STICK :public Object
+{
+public:
+	STICK() = default;
+	STICK(const Object* other) : Object(*other) {}
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+};
+class LAMP :public Object
+{
+public:
+	LAMP() = default;
+	LAMP(const Object* other) : Object(*other) {}
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+};
+class EAGLE :public Object
+{
+public:
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+};
+class COIN :public Object
+{
+public:
+	COIN() = default;
+	COIN(const Object* other) : Object(*other) {}
+	void self_load(const char *file, int pos1, int pos2, int posx, int posy, const char* sfile, bool Is_move);
+};
