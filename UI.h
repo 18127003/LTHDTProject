9c33524cf0a77b6d476d4aeee28a5dc5e0ce8e06
minhbibeobu12345 <51192411+minhbibeobu12345@users.noreply.github.com>
2019-08-04 17:19:57 +0700

    Add files via upload

--- UI.h
+++ UI.h
@@ -1,23 +1,19 @@
 #pragma once
-#include "Genio.h"
+#include "SDLexe.h"
 class UI
 {
-private:
+protected:
 	G_Rect position;
 	G_Texture* texture;
 public:
 
 	UI();
-	~UI();
-	friend class Game;
+	virtual ~UI();
 	friend class ConsoleCtrl;
-	void self_load(G_Font* font,const char* cont, int ft1, int ft2, int ft3, int pos1, int pos2, int pos3, int pos4);
-	void self_load(const char*file, int pos1, int pos2, int pos3, int pos4);
+	virtual void self_load(const char*file, int pos1, int pos2, int pos3, int pos4);
 	int getPos(char dim);
-	void self_update(G_Font* font, char* chars);
 	void self_draw();
 	void self_draw(G_Rect pos);
 	void settexture(G_Texture* txture);
 	G_Texture* Get_texture() { return texture; }
 };
-
